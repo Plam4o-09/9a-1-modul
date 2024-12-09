b17c245a4fd7c0bc2703935a3692485a91fffa2a
Plam4o-09 <plamenduralijski@gmail.com>
2024-12-09 13:24:05 +0200

    Add files via upload

---
+++ rabota_v_klas/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace rabota_v_klas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {1, 3 , 44, 12 };
+            LinkedList<int> numbers = new LinkedList<int>(nums);
+            SrednoAretmetichno(numbers);
+            Console.WriteLine(numbers);
+
+        }
+
+        static void SrednoAretmetichno(LinkedList<int> numbers) 
+        { 
+             numbers.Average();
+        }
+    }
+}
